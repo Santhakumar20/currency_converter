c0b1bbfeed126613e7c1fd440556c0a05d04fab3
Gowtham <49744195+gowtham0210@users.noreply.github.com>
2024-03-13 19:01:30 +0530

    App completed

--- app/src/main/java/com/example/currency_converter/MainActivity.java
+++ app/src/main/java/com/example/currency_converter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.currency_converter;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,15 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void convertToDollar(View view){
+        EditText editText = (EditText) findViewById(R.id.edtText);
+
+        int rupees = Integer.parseInt(editText.getText().toString());
+        int dollars = 82;
+
+        double result = rupees * dollars ;
+
+        Toast.makeText(MainActivity.this, Double.toString(result), Toast.LENGTH_LONG).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
